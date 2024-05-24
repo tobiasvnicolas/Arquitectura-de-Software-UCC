3b3c211151c7f69fdef3f5f04dac523fe58cab11
mateoveda <2214715@ucc.edu.ar>
2024-05-24 16:10:34 -0300

    xd

--- backend/main.go
+++ backend/main.go
@@ -10,8 +10,10 @@ func main() {
 	engine := gin.New()
 	app.MapRoutes(engine) //mapea las rutas de la aplicación
 	engine.Run(":8080")   //40:00 Explicación de quien llama a quien
+	enginer := 4
 }
 
 //47:15 Explicación BindJson
 //el Domain se usa para declarar las estructuras
 ///53:40 explicación de como pasarle el dato alusuario desde el controller
+//jdnfugbrd
