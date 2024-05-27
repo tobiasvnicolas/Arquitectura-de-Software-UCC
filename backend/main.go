095bc67cf76eac5585070432f4f51b7608246cc1
mateoveda <2214715@ucc.edu.ar>
2024-05-27 15:00:11 -0300

    Modifique los Dominios

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"Arquitectura-de-Software-UCC/backend/app"
 	"Arquitectura-de-Software-UCC/backend/db"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.StartDbEngine()
 	app.StartRoute()
 
+	//engine := gin.New()
+	//enrutador.MapUrls(engine)
+	//engine.Run(":8080")
 }
