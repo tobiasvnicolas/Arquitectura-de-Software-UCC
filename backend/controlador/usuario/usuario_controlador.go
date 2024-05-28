30fc02ec223449659e9febc02481bff9aaefbc24
Luciana Hueda <2203601@ucc.edu.ar>
2024-05-28 09:29:09 -0300

    login

--- backend/controlador/usuario/usuario_controlador.go
+++ backend/controlador/usuario/usuario_controlador.go
@@ -1,2 +1,48 @@
 package controlador
 
+import(
+	"github.com/gin-gonic/gin"
+	"Arquitectura-de-Software-UCC/backend/dominio/usuario"
+	"net/http"
+	servicio "Arquitectura-de-Software-UCC/backend/servicios/usuario"
+
+)
+
+func Login(c *gin.Context){
+	var request dominio.LoginRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil{
+		c.JSON(http.StatusBadRequest, dominio.Resultado{
+			Mensaje: fmt,Sprintf("Request invalido: %s", err.Error()),
+		})
+		return
+	}
+
+	token, err := servicios.Login(request.Email, request.Password)
+	if err != nil{
+		c.JSON(http.StatusUnathoride, dominio.Resultado{
+			Mensaje: fmt.Sprintf("Login no autorizado: %s", err.Error()),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, dominio.LoginResponse{
+		Token: token,
+	})
+}
+
+func GetUsuariobyEmail(c *gin.Context){
+
+email, _ := strconv.Atoi(c.Param("email"))
+var usuarioData dominio.UsuarioData
+
+usuarioData, err := servicio.usuario.GetUsuariobyEmail(email)
+
+if err != nil{
+	c.JSON(err.Status(),err)
+	return
+}
+
+c.JSON(http.StatusOK, usuarioData)
+
+}
