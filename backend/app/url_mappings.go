75be58d9fbe62a6135c8d269b33a00e7846a9d2b
mateoveda <2214715@ucc.edu.ar>
2024-05-31 20:02:32 -0300

    Arreglé un error por copiar mal

--- backend/app/url_mappings.go
+++ backend/app/url_mappings.go
@@ -1,1 +1,25 @@
 package app
+
+import (
+	cursoControlador "Arquitectura-de-Software-UCC/backend/controlador/cursos"
+	usuarioControlador "Arquitectura-de-Software-UCC/backend/controlador/usuario"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func mapUrls() {
+
+	//USUARIOS
+	router.POST("/login", usuarioControlador.Login)
+	router.GET("/usuario/:email", usuarioControlador.GetUsuariobyEmail)
+	router.POST("/usuario", usuarioControlador.CrearUsuario)
+
+	// CURSOS
+	router.POST("/cursos", cursoControlador.CrearCurso)
+	router.GET("/cursos/:id", cursoControlador.GetCursoById)
+
+	// INSCRIPCION
+
+	log.Info("Finishing mappings configurations")
+
+}
--- backend/clientes/usuario/usuarios_clientes.go
+++ backend/clientes/usuario/usuarios_clientes.go
@@ -1,1 +1,50 @@
-package usuarios
+package clientes
+
+import (
+	dao "Arquitectura-de-Software-UCC/backend/dao"
+
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
+
+type usuarioCliente struct{}
+
+type UsuarioClienteInterface interface {
+	GetUsuariobyEmail(email string) (dao.Usuario, error)
+	CrearUsuario(user dao.Usuario) dao.Usuario
+}
+
+var (
+	UsuarioCliente UsuarioClienteInterface
+)
+
+func init() {
+	UsuarioCliente = &usuarioCliente{}
+}
+
+func (s *usuarioCliente) GetUsuariobyEmail(email string) (dao.Usuario, error) {
+	var usuario dao.Usuario
+
+	result := Db.Where("email = ?", email).First(&usuario)
+	if result.Error != nil {
+		return usuario, result.Error
+	}
+	log.Debug("Usuario: ", usuario)
+
+	return usuario, nil
+}
+
+func (s *usuarioCliente) CrearUsuario(user dao.Usuario) dao.Usuario {
+
+	result := Db.Create(&user)
+
+	if result.Error != nil {
+		log.Error("")
+	}
+
+	log.Debug("Usuario Creado: ", user.UsuarioID)
+	return user
+
+}
