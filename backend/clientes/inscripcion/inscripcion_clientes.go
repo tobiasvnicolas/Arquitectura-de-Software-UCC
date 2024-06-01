93e8225f84de681d621fdc5872d453e72c7fdca9
mateoveda <2214715@ucc.edu.ar>
2024-06-01 12:27:45 -0300

    se agregaron cosas en el client de inscripcion y se crea el GetUsuariobyID

--- backend/clientes/inscripcion/inscripcion_clientes.go
+++ backend/clientes/inscripcion/inscripcion_clientes.go
@@ -1,1 +1,24 @@
 package inscripcion
+
+import (
+	"Arquitectura-de-Software-UCC/backend/dao"
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
+
+type inscripcionCliente struct{}
+
+type inscripcionClienteInterface interface {
+	CrearInscripcion(inscripcion dao.Inscripcion) dao.Inscripcion
+	GetInscripcionById(id int64) (dao.Inscripcion, error)
+}
+
+var (
+	InscripcionCliente inscripcionCliente
+)
+
+func init() {
+	inscripcionCliente = &inscripcionCliente{}
+}
